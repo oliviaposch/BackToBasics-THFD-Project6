bb48d2c885568d7b73abe251e9915ffb82e56329
olivia posch <pantera11@hotmail.ch>
2020-04-17 20:11:33 +0200

    set the game display and create a checkletter function

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ overlay.addEventListener('click', (e) => {
 //phrases Array
 const phrasesArr = [
     'Easy Peasy',
-    'The ball is in your court',
+    'The ball is in your cort',
     'hit the hay',
     'A penny for your thoughts',
     'A piece of cake'
@@ -25,8 +25,64 @@ const phrasesArr = [
  * the function an array as a parameter / returns an array of characters
  */
 function getRandomPhraseAsArray(arr){
-    //do stuff to any arr that is passed in
+    for (let i = arr.length - 1; i > 0; i--) {
+
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        
+    }
+    let elemToShow = arr[0].replace(/[^\w\s]/gi, ''); 
+    let arrOfCharacters = elemToShow.split('');
+    return arrOfCharacters;
+}
+
+/**
+ * Loops through an Arr. of Char
+ * for each Char in the Arr. create a LI
+ * put the Char inside of the LI
+ * append the LI to the #phrase UL in HTML
+ * If the Char in the Arr is a Letter and not Space, add class 'letter' to LI
+ */
+function addPhraseToDisplay(arr){
+    const ulList = document.querySelector('#phrase ul');
+    
+    for (let i = 0; i < arr.length; i++ ) {
+        let createLi = document.createElement('li'); 
+        createLi.innerText = arr[i];
+        if (createLi.innerText !== ' ') {
+            createLi.className = 'letter';
+        }else{
+            createLi.style.width = '1em';
+        }
+        ulList.appendChild(createLi);
+    }
+    return ulList;
+}
+const phrasesArrChars = getRandomPhraseAsArray(phrasesArr);
+addPhraseToDisplay(phrasesArrChars);
+
+/**
+ * FUNCTION HAVE ONE PARAMETER: the Btn the player has clicked
+ * function get all Elements with class Letter
+ * loop over the letters and check:
+ *  if they match the Letter in the Btn the player has chosen
+ * if there's match , add 'show' class to the list item
+ * store the matching letter inside of a variable, and return that letter
+ * if a match wasn't found, the function return null 
+ */
+function checkLetter(userGessBtn){
+
+    const classLetterLi = document.getElementsByClassName('letter'); //console.log(classLetterLi);
+
+    for (let i = 0; i < classLetterLi.length; i++) {
+        if( classLetterLi[i].innerText.toLowerCase === userGessBtn.toLowerCase){
+            //add show class just to the matched letter?
+            classLetterLi[i].classList.add('show');
+            console.log(classLetterLi[i]);
+        }
+        
+    }
+    return classLetterLi; 
 
 }
-//calling getRandomPhraseAsArray should have the phrasesArr array as an argument
-//console.log(phrasesArr); 
+checkLetter('E');
