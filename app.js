83e87e2c81a7ad3e289687466a8ca428d2b3b9a6
Olivia <pantera11@hotmail.ch>
2020-04-23 11:48:15 +0200

    reset random phrase in the function resetGame

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let missed = 0; // to track the guesses the player has missed
 const keyBoard = document.getElementById('qwerty'); 
 const scoreBoard = document.getElementsByClassName('tries');
 const overlay = document.getElementById('overlay');
-
+let letterFound = '';
 
 //phrases Array
 const phrasesArr = [
@@ -15,44 +15,7 @@ const phrasesArr = [
 ];
 
 /**
- * Reset Function
- */
-function resetGame(){
- 
-    /*
-    //remove class chosen
-    //remove Class show
-    //missed class set to 0
-    
-    const removeShowClass = document.getElementsByClassName('keyrow'); //array of classes
-     for (let index = 0; index < removeShowClass.length; index++) {
-         const element = removeShowClass[index].children; 
-         if(element.classList.contains('chosen')){
-             element.classList.remove('chosen');
-         }else{
-             console.log(' no foundS');
-         }
-         
-         
-     }
-     console.log(removeShowClass.length); */
- 
- }
- 
- //eventListener start game
-overlay.addEventListener('click', (e) => {
-    if(e.target.className == 'btn__reset'){
-        overlay.style.display = 'none';
-        //reset function
-    }
-    
-});
-
-/**
  * randomly phrasesArr array
- * choose a phrase
- * split choosed phrase into a new array of characters 
- * the function an array as a parameter / returns an array of characters
  */
 function getRandomPhraseAsArray(arr){
     for (let i = arr.length - 1; i > 0; i--) {
@@ -68,10 +31,7 @@ function getRandomPhraseAsArray(arr){
 
 /**
  * Loops through an Arr. of Char
- * for each Char in the Arr. create a LI
- * put the Char inside of the LI
- * append the LI to the #phrase UL in HTML
- * If the Char in the Arr is a Letter and not Space, add class 'letter' to LI
+ * create a LI | append the LI to the #phrase UL in HTML
  */
 function addPhraseToDisplay(arr){
     const ulList = document.querySelector('#phrase ul');
@@ -88,17 +48,38 @@ function addPhraseToDisplay(arr){
     }
     return ulList;
 }
-const phrasesArrChars = getRandomPhraseAsArray(phrasesArr);
-addPhraseToDisplay(phrasesArrChars);
+
+
+/**
+ * Reset Function
+ */
+function resetGame(){
+    const phrasesArrChars = getRandomPhraseAsArray(phrasesArr);
+    addPhraseToDisplay(phrasesArrChars);
+    
+    letterFound = '';
+    missed = 0;
+ 
+ }
+
+/**
+ *  eventListener start game
+ */ 
+overlay.addEventListener('click', (e) => {
+    if(e.target.className == 'btn__reset'){
+        overlay.style.display = 'none';
+        //reset function
+        resetGame();
+    }
+    
+});
+
+
 
 /**
  * FUNCTION HAVE ONE PARAMETER: the Btn the player has clicked
  * function get all Elements with class Letter
- * loop over the letters and check:
- *  if they match the Letter in the Btn the player has chosen
- * if there's match , add 'show' class to the list item
- * store the matching letter inside of a variable, and return that letter
- * if a match wasn't found, the function return null 
+ * loop over the letters and check and compare with the UserGess choises
  */
 function checkLetter(userGessBtn){
 
@@ -128,14 +109,11 @@ keyBoard.addEventListener('click', (e) => {
         e.target.className = 'chosen';
         if(e.target.classList.contains('chosen')){
             e.target.setAttribute('disabled', true);
-            let letterFound = checkLetter(e.target.innerText); 
+            letterFound = checkLetter(e.target.innerText); 
             
              /**
-            * Count the missed guesses
-            * if checkletter func. return null: wrong gessed letter
-            * 1. statement to check the value of the letterfound Var. 
-            * if valeu null : remove one of the tries from scoreboard
-            * we need the missed Var. (remove try from scoreboard means increase the missedCount by 1)
+            * Count the missed guesses 
+            * if valeu null : remove one of the tries from scoreboard | and increase missed class
             */
             if(letterFound === null){ 
                 missed++;
@@ -168,9 +146,8 @@ keyBoard.addEventListener('click', (e) => {
 
 /**
  * checkWin Func.
- * is number of class SHOW = numeber of letters with class Letters?
- * if iqual show win overlay ID with win:CLASS and text 'Win'exemplo
- * otherwise, if number of misses  >= 5 SHOW overlay 'lose' CLASS and text 'lose' exemplo
+ * is number of class SHOW equal number of letters with class Letters?
+ * if number of misses  === 5 SHOW overlay 'lose' 
  */
 function checkWin() {
     const showClassArr = document.getElementsByClassName('show');
@@ -181,12 +158,13 @@ function checkWin() {
         overlay.style.display = 'flex';
         overlay.firstElementChild.innerText = 'Congratulations you won the Game!';
         overlay.lastElementChild.innerText = 'Play again!';
-        //resetGame();
+        
     }
     if(missed === 5){
         overlay.className = 'lose';
         overlay.style.display = 'flex';
         overlay.firstElementChild.innerText = 'Game over!';
         overlay.lastElementChild.innerText = 'Try again!';
+        
     }
 }
